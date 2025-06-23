fee344e2d8436bbe1efa26333a70f3c162da1f91
MernNasa <mernnasa@gmail.com>
2025-06-23 23:15:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ ConnectDB()
 app.use(express.json())
 app.use(
     cors({
-      origin:['http://localhost:5173',"http://127.0.0.1:5500"], // ✅ Allow frontend URL only (NOT '*')
+      origin:['http://localhost:5174',"http://127.0.0.1:5500"], // ✅ Allow frontend URL only (NOT '*')
       credentials: true, // ✅ Allow sending cookies
     })
   );
